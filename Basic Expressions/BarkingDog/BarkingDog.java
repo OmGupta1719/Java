4ebf97582e84a8361b1eead3e139820535c96a94
OmGupta1719 <127668780+OmGupta1719@users.noreply.github.com>
2024-02-04 11:26:24 +0530

    Update BarkingDog.java

--- Basic Expressions/BarkingDog/BarkingDog.java
+++ Basic Expressions/BarkingDog/BarkingDog.java
@@ -3,7 +3,7 @@ public class BarkingDog {
   //Solution method
      public static boolean shouldWakeUp(boolean barking,int hourOfDay){
         if(hourOfDay>=0 && hourOfDay<=23){
-            if(barking && hourOfDay<8 || hourOfDay>22){  
+            if(barking && hourOfDay<8 || hourOfDay>22){      //barking==true can be simply written as barking
                 return true;
             }
             else{
